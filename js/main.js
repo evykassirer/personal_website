411e027f2badc23a2f8f2821110d7ebae78e2c27
Evy Kassirer <evy.kassirer@gmail.com>
2014-09-28 13:48:34 -0400

    removes things from menuwhen height is too short

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 var dropdown = document.getElementsByClassName('clickopen');
 var list = document.getElementsByClassName('clicklist');
 if (list)
-	var links = list[0].children; 
+	var parent = list[0]
+	var links = parent.children; 
 if (dropdown && list && links) {
 	dropdown[0].onclick = function() {
-		for(var i = 0; i < links.length; i++) {
-			if (links[i].style.display == "block") {
-				links[i].style.display = "none";
-			} else { 
-				links[i].style.display = "block";
+		var open = parent.className.indexOf("open");
+		if (open == -1) {
+			for(var i = 0; i < links.length; i++) {
+				if ((window.innerHeight < 400 && links[i].className == "removefirst")){
+					links[i].style.display = "none";
+				} 
+				else if ((window.innerHeight < 360 && links[i].className == "removesecond")){
+					links[i].style.display = "none";
+				} else { 
+					links[i].style.display = "block";
+				}
 			}
+			parent.className = parent.className + " open";
+		}
+		else {
+			for(var i = 0; i < links.length; i++) {
+				links[i].style.display = "none";
+			}
+			parent.className =  parent.className.substring(0,open); 
 		}
 	}
 }
